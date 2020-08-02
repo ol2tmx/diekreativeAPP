30c0c243bd2bb80b1bbc887a73c2e1592a2091d3
Christian Wöhr <developer@xetan.de>
2020-08-02 19:36:36 +0200

    adapted media issue

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -14,23 +14,7 @@ export class Tab1Page {
 
 // Play audio
 //
-public playAudio(url) {
-  // Play the audio file at url
-  var my_media = new Media(url,
-      // success callback
-      function () { console.log("playAudio():Audio Success"); },
-      // error callback
-      function (err) { console.log("playAudio():Audio Error: " + err); }
-  );
 
-  // Play audio
-  my_media.play();
-
-  // Pause after 10 seconds
-  setTimeout(function () {
-      my_media.pause();
-  }, 10000);
-}
 
   public async presentModal() {
     const modal = await this.modalController.create({
