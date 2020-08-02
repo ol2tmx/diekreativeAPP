a3d2c90f1cfa436a11b1215c6c576fd856405bb2
phil56393 <phmo@freakmail.de>
2020-08-02 15:23:42 +0200

    Added slideshow

--- src/app/slide-show/slide-show.component.ts
+++ src/app/slide-show/slide-show.component.ts
@@ -8,157 +8,74 @@ import { ModalController } from '@ionic/angular';
 })
 export class SlideShowComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit() {}
-
-}
-
-@Component({
-  selector: 'slides-example',
-  template: `
-    <ion-content>
-      <ion-slides pager="true" [options]="slideOpts">
-        <ion-slide>
-          <h1>Slide 1</h1>
-        </ion-slide>
-        <ion-slide>
-          <h1>Slide 2</h1>
-        </ion-slide>
-        <ion-slide>
-          <h1>Slide 3</h1>
-        </ion-slide>
-      </ion-slides>
-    </ion-content>
-  `
-})
-export class SlideExample {
   // Optional parameters to pass to the swiper instance.
   // See http://idangero.us/swiper/api/ for valid options.
+
   slideOpts = {
-    initialSlide: 2,
-    speed: 1000
-  };
+      speed: 400, // in ms
+      on: {
+        beforeInit() {
+          const swiper = this;
+          swiper.classNames.push(`${swiper.params.containerModifierClass}fade`);
+          const overwriteParams = {
+            slidesPerView: 1,
+            slidesPerColumn: 1,
+            slidesPerGroup: 1,
+            watchSlidesProgress: true,
+            spaceBetween: 0,
+            virtualTranslate: true,
+          };
+          swiper.params = Object.assign(swiper.params, overwriteParams);
+          swiper.params = Object.assign(swiper.originalParams, overwriteParams);
+        },
+        setTranslate() {
+          const swiper = this;
+          const { slides } = swiper;
+          for (let i = 0; i < slides.length; i += 1) {
+            const $slideEl = swiper.slides.eq(i);
+            const offset$$1 = $slideEl[0].swiperSlideOffset;
+            let tx = -offset$$1;
+            if (!swiper.params.virtualTranslate) tx -= swiper.translate;
+            let ty = 0;
+            if (!swiper.isHorizontal()) {
+              ty = tx;
+              tx = 0;
+            }
+            const slideOpacity = swiper.params.fadeEffect.crossFade
+              ? Math.max(1 - Math.abs($slideEl[0].progress), 0)
+              : 1 + Math.min(Math.max($slideEl[0].progress, -1), 0);
+            $slideEl
+              .css({
+                opacity: slideOpacity,
+              })
+              .transform(`translate3d(${tx}px, ${ty}px, 0px)`);
+          }
+        },
+        setTransition(duration) {
+          const swiper = this;
+          const { slides, $wrapperEl } = swiper;
+          slides.transition(duration);
+          if (swiper.params.virtualTranslate && duration !== 0) {
+            let eventTriggered = false;
+            slides.transitionEnd(() => {
+              if (eventTriggered) return;
+              if (!swiper || swiper.destroyed) return;
+              eventTriggered = true;
+              swiper.animating = false;
+              const triggerEvents = ['webkitTransitionEnd', 'transitionend'];
+              for (let i = 0; i < triggerEvents.length; i += 1) {
+                $wrapperEl.trigger(triggerEvents[i]);
+              }
+            });
+          }
+        },
+      }
+    }
+
   constructor() {}
 }
 
-const slideOpts = {
-  on: {
-    beforeInit() {
-      const swiper = this;
-      swiper.classNames.push(`${swiper.params.containerModifierClass}fade`);
-      const overwriteParams = {
-        slidesPerView: 1,
-        slidesPerColumn: 1,
-        slidesPerGroup: 1,
-        watchSlidesProgress: true,
-        spaceBetween: 0,
-        virtualTranslate: true,
-      };
-      swiper.params = Object.assign(swiper.params, overwriteParams);
-      swiper.params = Object.assign(swiper.originalParams, overwriteParams);
-    },
-    setTranslate() {
-      const swiper = this;
-      const { slides } = swiper;
-      for (let i = 0; i < slides.length; i += 1) {
-        const $slideEl = swiper.slides.eq(i);
-        const offset$$1 = $slideEl[0].swiperSlideOffset;
-        let tx = -offset$$1;
-        if (!swiper.params.virtualTranslate) tx -= swiper.translate;
-        let ty = 0;
-        if (!swiper.isHorizontal()) {
-          ty = tx;
-          tx = 0;
-        }
-        const slideOpacity = swiper.params.fadeEffect.crossFade
-          ? Math.max(1 - Math.abs($slideEl[0].progress), 0)
-          : 1 + Math.min(Math.max($slideEl[0].progress, -1), 0);
-        $slideEl
-          .css({
-            opacity: slideOpacity,
-          })
-          .transform(`translate3d(${tx}px, ${ty}px, 0px)`);
-      }
-    },
-    setTransition(duration) {
-      const swiper = this;
-      const { slides, $wrapperEl } = swiper;
-      slides.transition(duration);
-      if (swiper.params.virtualTranslate && duration !== 0) {
-        let eventTriggered = false;
-        slides.transitionEnd(() => {
-          if (eventTriggered) return;
-          if (!swiper || swiper.destroyed) return;
-          eventTriggered = true;
-          swiper.animating = false;
-          const triggerEvents = ['webkitTransitionEnd', 'transitionend'];
-          for (let i = 0; i < triggerEvents.length; i += 1) {
-            $wrapperEl.trigger(triggerEvents[i]);
-          }
-        });
-      }
-    },
-  }
-}
 
 
-// const slideOpts = {
-//   on: {
-//     beforeInit() {
-//       const swiper = this;
-//       swiper.classNames.push(`${swiper.params.containerModifierClass}fade`);
-//       const overwriteParams = {
-//         slidesPerView: 1,
-//         slidesPerColumn: 1,
-//         slidesPerGroup: 1,
-//         watchSlidesProgress: true,
-//         spaceBetween: 0,
-//         virtualTranslate: true,
-//       };
-//       swiper.params = Object.assign(swiper.params, overwriteParams);
-//       swiper.params = Object.assign(swiper.originalParams, overwriteParams);
-//     },
-//     setTranslate() {
-//       const swiper = this;
-//       const { slides } = swiper;
-//       for (let i = 0; i < slides.length; i += 1) {
-//         const $slideEl = swiper.slides.eq(i);
-//         const offset$$1 = $slideEl[0].swiperSlideOffset;
-//         let tx = -offset$$1;
-//         if (!swiper.params.virtualTranslate) tx -= swiper.translate;
-//         let ty = 0;
-//         if (!swiper.isHorizontal()) {
-//           ty = tx;
-//           tx = 0;
-//         }
-//         const slideOpacity = swiper.params.fadeEffect.crossFade
-//           ? Math.max(1 - Math.abs($slideEl[0].progress), 0)
-//           : 1 + Math.min(Math.max($slideEl[0].progress, -1), 0);
-//         $slideEl
-//           .css({
-//             opacity: slideOpacity,
-//           })
-//           .transform(`translate3d(${tx}px, ${ty}px, 0px)`);
-//       }
-//     },
-//     setTransition(duration) {
-//       const swiper = this;
-//       const { slides, $wrapperEl } = swiper;
-//       slides.transition(duration);
-//       if (swiper.params.virtualTranslate && duration !== 0) {
-//         let eventTriggered = false;
-//         slides.transitionEnd(() => {
-//           if (eventTriggered) return;
-//           if (!swiper || swiper.destroyed) return;
-//           eventTriggered = true;
-//           swiper.animating = false;
-//           const triggerEvents = ['webkitTransitionEnd', 'transitionend'];
-//           for (let i = 0; i < triggerEvents.length; i += 1) {
-//             $wrapperEl.trigger(triggerEvents[i]);
-//           }
-//         });
-//       }
-//     },
-//   }
-// }
+
--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
-import { SlideShowComponent } from './slide-show.component';
+import { SlideShowComponent } from '../slide-show/slide-show.component'
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
@@ -16,6 +16,6 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     ExploreContainerComponentModule,
     Tab1PageRoutingModule
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab1Page,SlideShowComponent]
 })
 export class Tab1PageModule {}
