07d3e15b45be46e3a754df780593a1036e052009
Annemarlena <69078710+Annemarlena@users.noreply.github.com>
2020-08-02 18:05:17 +0200

    Kacheln mit Hintergrund

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
+import { GridHomescreenComponent } from '../grid-homescreen/grid-homescreen.component';
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 
 @NgModule({
@@ -15,6 +15,6 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     ExploreContainerComponentModule,
     Tab1PageRoutingModule
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab1Page,GridHomescreenComponent]
 })
 export class Tab1PageModule {}
