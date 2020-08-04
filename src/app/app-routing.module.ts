6fa2a23569baa33487fe1f4ae24419f8e87572ea
Christian Wöhr <developer@xetan.de>
2020-08-04 10:45:51 +0200

    Links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -42,18 +42,6 @@ const routes: Routes = [
     path: 'lifegroups',
     loadChildren: () => import('./lifegroups/lifegroups.module').then( m => m.LifegroupsPageModule)
   }
-  {
-    path: 'ueber-uns',
-    loadChildren: () => import('./ueber-uns/ueber-uns.module').then( m => m.UeberUnsPageModule)
-  },
-  {
-    path: 'gottesdienste',
-    loadChildren: () => import('./gottesdienste/gottesdienste.module').then( m => m.GottesdienstePageModule)
-  },
-  {
-    path: 'tagundnacht',
-    loadChildren: () => import('./tagundnacht/tagundnacht.module').then( m => m.TagundnachtPageModule)
-  }
 
 
 
